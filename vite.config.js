df4b377c80ece591b544332030ad42c2b7777945
Nikita Korobkov <nikkorobk@gmail.com>
2026-01-05 21:12:02 -0500

    Add gh pages deploy config

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 
 export default defineConfig({
+  base : '/poroshki-quiz/',
   plugins: [preact()],
   server: {
     port: 3000
+  },
+  build: {
+    outDir: 'dist'
   }
 });
 
